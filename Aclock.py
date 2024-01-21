6ab8e875364aa875b62fc85789db49cca95afa73
Alireza <alireza.sad.ger@gmail.com>
2024-01-21 22:57:41 +0330

    the clock hands are add

--- Aclock.py
+++ Aclock.py
@@ -1,4 +1,6 @@
 import pygame
+from math import radians, sin, cos
+from datetime import datetime
 
 
 class Clock():
@@ -16,15 +18,61 @@ class Clock():
         pygame.display.set_caption("Analog Clock")
         self.clock = pygame.time.Clock()
 
+    def numbers(self, number, size, positsion):
+        font = pygame.font.SysFont("calibri", size, True, False)
+        text = font.render(str(number), True, self.white)
+        text_rect = text.get_rect(center=positsion)
+        self.screen.blit(text, text_rect)
+
+    def polar_to_cartesian(self, r, theta):
+        x = r * sin(radians(theta))
+        y = r * cos(radians(theta))
+        return self.width//2 + x, self.height//2 - y
+
     def draw_circle(self, screen):
         # main circle
         pygame.draw.circle(self.screen, self.white,
                            self.center, self.clock_radius - 10, 5)
-        
-        #central circle
+
+        # central circle
         pygame.draw.circle(self.screen, self.white,
                            self.center, 10)
 
+        for number in range(1, 13):
+            self.numbers(number, 60, self.polar_to_cartesian(
+                self.clock_radius - 80, number * 30))
+
+        for number in range(0, 360, 6):
+            if number % 5:
+                pygame.draw.line(self.screen, self.white, self.polar_to_cartesian(
+                    self.clock_radius-15, number), self.polar_to_cartesian(self.clock_radius-30, number), 2)
+            else:
+                pygame.draw.line(self.screen, self.white, self.polar_to_cartesian(
+                    self.clock_radius-15, number), self.polar_to_cartesian(self.clock_radius-35, number), 6)
+
+    def draw_clock_hand(self):
+        curent_time = datetime.now()
+        second = curent_time.second
+        minute = curent_time.minute
+        hour = curent_time.hour
+
+        #hour
+        R = self.clock_radius -190
+        theta = (hour + minute/60 + second/3600) * (360 / 12)
+        pygame.draw.line(self.screen, self.white, self.center, self.polar_to_cartesian
+                         (R, theta), 14)
+        
+        #minute
+        M = self.clock_radius -150
+        theta = ( minute + second/60) * (360 / 60)
+        pygame.draw.line(self.screen, self.white, self.center, self.polar_to_cartesian
+                         (R, theta), 10)
+        
+        #second
+        S = self.clock_radius -100
+        theta = second * (360 / 60)
+        pygame.draw.line(self.screen, self.white, self.center, self.polar_to_cartesian
+                         (R, theta), 4)
 
     def run(self):
         while True:
@@ -33,6 +81,7 @@ class Clock():
                     exit()
             self.screen.fill(self.blue)
             self.draw_circle(self.screen)
+            self.draw_clock_hand()
 
             pygame.display.update()
             self.clock.tick(self.FPS)
